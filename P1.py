a4a1fb8f74dd7266f150e8e30353ed40c438fa21
js012108 <js012108@gmail.com>
2023-03-28 06:41:39 -0500

    Primer Punto

--- P1.py
+++ P1.py
@@ -1,1 +1,8 @@
-conversion.py
+#Conversión de temparaturas.
+c=float(input('Ingrese la temperatura en Grados Celcius: '))
+f=((9/5)*c) + 32
+r=((c*1.8)+491.67)
+k=c+273.15
+print(c, 'es equivalente a', f,'Grados Fahrenheit')
+print(c, 'es equivalente a', r,'Grados Rankine')
+print(c, 'es equivalente a', k,'Grados Kelvin')
